aa6fe98e51a00fcec344a55c864e93c512c81e53
Nien Luong <tatsuyanin@gmail.com>
2020-06-14 15:14:12 +0700

    Updates

---
+++ precache-manifest.3499d68a3b9f0ab6bec0b206ef61f299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64948eb5802356b450c18ff5b7d91d5",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "bdde32dd2986ca3c8b2f",
+    "url": "/my-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4339b0a62d23245f2b39",
+    "url": "/my-portfolio/static/js/2.a789ae92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.a789ae92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdde32dd2986ca3c8b2f",
+    "url": "/my-portfolio/static/js/main.2915ff32.chunk.js"
+  },
+  {
+    "revision": "89b80f0061825c5bbaae",
+    "url": "/my-portfolio/static/js/runtime-main.ece6c2b1.js"
+  }
+]);
